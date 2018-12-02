1a41abadfa15c8a18f7f4e54dbca8a8c5d516ef3
Tyler Pottenger <tyler.pottenger@gmail.com>
2018-12-02 16:09:01 -0500

    Solution for Day 2 puzzle

---
+++ src/day2/mod.rs
@@ -0,0 +1,59 @@
+use std::collections::HashMap;
+
+const INPUT: &str = include_str!("input");
+
+pub fn solve() {
+    println!("Day 2, Part 1: {}", part1(INPUT));
+    println!("Day 2, Part 2: {}", part2(INPUT));
+}
+
+fn part1(input: &str) -> usize {
+    let words = input.lines();
+
+    let num_two_dupes = words.clone()
+        .filter(|word| has_exact_num_of_dupes(word, 2))
+        .count();
+
+    let num_three_dupes = words
+        .filter(|word| has_exact_num_of_dupes(word, 3))
+        .count();
+
+    num_two_dupes * num_three_dupes
+}
+
+fn part2(input: &str) -> String {
+    let input_clone = input.clone();
+    for a in input.lines() {
+        for b in input_clone.lines() {
+            if words_differ_by_one(a, b) {
+                return common_letters(a, b);
+            }
+        }
+    }
+    String::from("")
+}
+
+fn has_exact_num_of_dupes(word: &str, num: usize) -> bool {
+    let mut counts = HashMap::with_capacity(26);
+    for c in word.chars() {
+        *counts.entry(c).or_insert(0) += 1;
+    }
+    counts.values().any(|&c| c == num)
+}
+
+fn words_differ_by_one(a: &str, b: &str) -> bool {
+    let num_differing = a.chars()
+        .zip(b.chars())
+        .filter(|(a, b)| a != b)
+        .count();
+
+    num_differing == 1
+}
+
+fn common_letters(a: &str, b: &str) -> String {
+    a.chars()
+        .zip(b.chars())
+        .filter(|(a, b)| a == b)
+        .map(|(a, _)| a)
+        .collect()
+}
