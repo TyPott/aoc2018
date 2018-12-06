b38021d66f94f3454de3306a94df94e6d3f77f0c
Tyler Pottenger <tyler.pottenger@gmail.com>
2018-12-05 20:42:34 -0500

    Solution for Day 5 puzzle

---
+++ day5/src/main.rs
@@ -0,0 +1,29 @@
+static INPUT: &str = include_str!("input");
+
+fn main() {
+    println!("Day 5, Part 1: {}", part1(INPUT.trim().chars()));
+    println!("Day 5, Part 2: {}", part2(INPUT).unwrap());
+}
+
+fn part1<T: Iterator<Item = char>>(s: T) -> usize {
+    s.fold(Vec::new(), |mut cache, c| {
+        match cache.last() {
+            None => cache.push(c),
+            Some(&l) => if l != c && l.eq_ignore_ascii_case(&c) {
+                cache.pop();
+            } else {
+                cache.push(c);
+            },
+        }
+        cache
+    }).len()
+}
+
+fn part2(s: &str) -> Option<usize> {
+    Some((b'A'..=b'Z')
+        .map(|b| b as char)
+        .map(|c| {
+            part1(s.trim().chars().filter(|l| !l.eq_ignore_ascii_case(&c)))
+        })
+        .min()?)
+}
